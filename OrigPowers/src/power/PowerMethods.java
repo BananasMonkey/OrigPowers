a3f9de9fc0b49a3a88fd6d2c6adb5517f23398b7
John <81330397+BananasMonkey@users.noreply.github.com>
2024-05-25 10:56:53 -0700

    Release 1.0 (Phantom)

--- OrigPowers/src/power/PowerMethods.java
+++ OrigPowers/src/power/PowerMethods.java
@@ -1,5 +1,6 @@
 package power;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.potion.PotionEffect;
@@ -9,15 +10,19 @@ public class PowerMethods {
 	public class Power_Phantom implements PowerMethodsInterface {
 		public void onSneak(PlayerToggleSneakEvent e) {
 			if (e.isSneaking()) {
-				PotionEffect invisible = new PotionEffect(PotionEffectType.INVISIBILITY,15,1,false,false,false);
+				PotionEffect invisible = new PotionEffect(PotionEffectType.INVISIBILITY,Integer.MAX_VALUE,1,false,false,false);
 				e.getPlayer().addPotionEffect(invisible);
+			} else {
+				e.getPlayer().removePotionEffect(PotionEffectType.INVISIBILITY);
 			}
 		}
 
 		@Override
 		public void onSwapHands(PlayerSwapHandItemsEvent e) {
+			Player player = e.getPlayer();
 			PotionEffect invisible = new PotionEffect(PotionEffectType.INVISIBILITY,15,1,false,false,false);
-			e.getPlayer().addPotionEffect(invisible);
+			player.addPotionEffect(invisible);
+			player.setVelocity(player.getLocation().getDirection().multiply(0.7D));
 		}
 	}
 }
