03d7302c7c5a82fbb8b1bb372c9eb7d9f6d8fc96
John <81330397+BananasMonkey@users.noreply.github.com>
2024-05-25 10:31:29 -0700

    Test1
    Hope it works!

--- OrigPowers/src/me/monkey/Main.java
+++ OrigPowers/src/me/monkey/Main.java
@@ -3,12 +3,13 @@ package me.monkey;
 import java.util.HashMap;
 
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
+//import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import power.Power;
@@ -16,7 +17,7 @@ import power.PowerMethods;
 import power.PowerMethodsInterface;
 
 public class Main extends JavaPlugin implements Listener{
-	static FileConfiguration config = Main.getPlugin(Main.class).getConfig();
+	//static FileConfiguration config = Main.getPlugin(Main.class).getConfig();
 	static HashMap<String, Power> playerPower = new HashMap<String, Power>();
     @Override
     public void onEnable() {
@@ -44,7 +45,14 @@ public class Main extends JavaPlugin implements Listener{
     	Player player = event.getPlayer();
     	Power power = playerPower.get(player.getName());
     	PowerMethodsInterface powerFunctions = power.getPowerObject();
-    	powerFunctions.swapHands(event); // code no no worky
+    	powerFunctions.onSwapHands(event);
+    }
+    @EventHandler
+    public static void sneak(PlayerToggleSneakEvent event) {
+    	Player player = event.getPlayer();
+    	Power power = playerPower.get(player.getName());
+    	PowerMethodsInterface powerFunctions = power.getPowerObject();
+    	powerFunctions.onSneak(event);
     }
 }   		 
     
